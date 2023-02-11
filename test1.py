e56a942008a394c21eb1bfd8095e43d192ec5c9f
liyangwei <2531222095@qq.com>
2023-02-11 23:03:28 +0800

    增加pytest_generate_tests的测试

---
+++ test1.py
@@ -0,0 +1,11 @@
+# names = ["var1", "var2"]
+
+# test_data = [1, 2]
+
+params_args = ["username", "password"]
+test_data = [["test1", "123"], ["test2", "456"]]
+
+
+def test(username, password):
+    print(username, password)
+    # assert param < 10
